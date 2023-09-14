3341e9044a667abefbc12253605d3d4d21a5f211
johnlima10 <141142799+johnlima10@users.noreply.github.com>
2023-09-14 11:49:06 -0300

    11:48(14/09)

--- padaria.sql
+++ padaria.sql
@@ -124,3 +124,54 @@ create table area_do_cafe(
 	foreign key (id_cliente) references cliente(id_cliente),
 	foreign key (id_de_mesa) references reserva_de_mesa(id_de_mesa)
 );
+
+-- Inserindo dados na tabela
+
+insert into padaria(id_da_padaria, nome, telefone)
+values
+(150, 'Padaria Bom Sabor', '98889-4433'),
+(151, 'Padaria Bom Jesus', '97889-7833')
+
+select * from padaria
+
+insert into cliente(id_cliente, nome, email, telefone, id_da_padaria)
+values
+(1, 'Jonathan Lima', 'john@hotmail.com', '98519-4433', 150),
+(2, 'Maria José', 'mariaj@yahoo.com', '92489-7833', 151)
+
+select * from cliente
+
+insert into produto(id_do_produto, nome, preco, descricao, ingrediente, id_da_padaria)
+values
+(25, 'Pão', 1.50, 'Pão Frances', 'farinha, fermento, sal', 150),
+(26, 'Bolo', 50.99, 'Bolo de chocolate trufado', 'farinha, ovo, chocolate, leite', 151)
+
+select * from produto
+
+insert into funcionario(id_do_funcionario, nome, id_da_padaria)
+values
+(1000, 'José Maria Nascimento', 150),
+(1001, 'Maria Eduarda Castro', 151)
+
+select * from funcionario
+
+insert into endereco(id_do_endereco, rua, numero, bairro, complemento, cep, cidade, uf, id_da_padaria)
+values
+(10, 'Rua São João', '22', 'Cidade Alta', '', 1256587, 'Natal', 'RN', 150),
+(11, 'Av. Salgado Filho', '582', 'Lagoa Nova', '', 1256590, 'Natal', 'RN', 151)
+
+select * from endereco
+
+insert into pedido(id_do_pedido, data_de_pedido, hora_do_pedido, quantidade)
+values
+(150, '2023/09/13', '08:20', 10),
+(151, '2023/09/13', '08:00', 1)
+
+select * from pedido
+
+insert into reserva_de_mesa(id_de_mesa, num_de_pessoa, preferencia, data_reserva, hora)
+values
+(23, '84988581245', 'Mesa interna', '2023/09/14', '15:00'),
+(24, '84987586874', 'Mesa externa', '2023/09/14', '10:30')
+
+select * from reserva_de_mesa
